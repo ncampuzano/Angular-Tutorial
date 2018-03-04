a11ea255ed00e9b0ea6c660e6b1319c609409fc8
Nicolás Campuzano <ncampuzano@users.noreply.github.com>
2018-03-03 19:32:57 -0500

    Add message and  services

--- campuzano-heroes/src/app/heroes/heroes.component.ts
+++ campuzano-heroes/src/app/heroes/heroes.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 
+import { HeroService } from '../hero.service';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -14,15 +16,20 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm'
   };
 
-  heroes = HEROES;
+  heroes : Hero[];
   selectedHero: Hero;
 
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-  constructor() { }
+  constructor(private heroService: HeroService) { 
+  }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
 }
